26f233b0dda23adf17e635d800ada01c78f49dcf
Mirislom <m.mirislom.m@gmail.com>
2024-07-08 17:54:02 +0500

    [+] Responsive navbar

--- js/swiper.js
+++ js/swiper.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Initialize Swiper
     const swiper = new Swiper('.swiper-container', {
-        direction: 'horizontal', // For vertical sliding
-        slidesPerView: 1, // Show one slide at a time
-        spaceBetween: 30, // Space between slides in pixels
-        mousewheel: true, // Enable mousewheel control
+        direction: 'horizontal',
+        slidesPerView: 1,
+        spaceBetween: 30,
+        mousewheel: true,
         pagination: {
-            el: '.swiper-pagination', // CSS selector for pagination container
-            clickable: true, // Make pagination bullets clickable
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
     });
 });
