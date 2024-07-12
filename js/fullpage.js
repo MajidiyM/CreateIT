48c1cea50f8890426f803a0c8b87e176dd85125f
Mirislom <m.mirislom.m@gmail.com>
2024-07-13 00:48:42 +0500

    [+] Added basic fullpage.js init

---
+++ js/fullpage.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //options here
+        autoScrolling: true,
+        scrollHorizontally: true,
+        navigation: true,
+        navigationPosition: 'right',
+        anchors: ['section1', 'section2', 'section3', 'section4'],
+        menu: '#menu'
+    });
+});
